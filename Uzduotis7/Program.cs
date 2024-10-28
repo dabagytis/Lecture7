bf19e43b0bd85e208aa1197f68e00be2b61e209d
Gytis Dabašinskas <g.dabasinskas@gmail.com>
2024-10-28 17:07:56 +0200

    Uzduotys 7 ir 8

---
+++ Uzduotis7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lecture7
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] masyvas = new int[5];
+            int suma = 0;
+
+            Console.WriteLine($"Iveskie {masyvas.Length} skaicius");
+            for (int i = 0; i < masyvas.Length; i++)
+            {
+                masyvas[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < masyvas.Length; i++)
+            {
+                if (masyvas[i] >= 0)
+                {
+                    suma = suma + masyvas[i];
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(suma);
+        }
+    }
+}
---
+++ Uzduotis8/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Lecture7
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] masyvas = new int[6];
+            int zeroCount = 0;
+            int numberCount = masyvas.Length - zeroCount;
+
+            Console.WriteLine($"Iveskie {masyvas.Length} skaicius");
+            for (int i = 0; i < masyvas.Length; i++)
+            {
+                masyvas[i] = int.Parse(Console.ReadLine());
+                if (masyvas[i] != 0)
+                {
+                    zeroCount++;
+                }
+            }
+
+            int[] naujasMasyvas = new int[masyvas.Length];
+            int j = 0;
+
+            for (int i = 0; i < masyvas.Length; i++)
+            {
+                if (masyvas[i] != 0)
+                {
+                    naujasMasyvas[j] = masyvas[i];
+                    j++;
+                }
+            }
+
+            for(int i = 0; i < masyvas.Length; i++)
+            {
+                Console.WriteLine(naujasMasyvas[i]);
+            }
+        }
+    }
+}
